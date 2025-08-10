938d806f0fe045a64952a73db14dfd1343ca3c19
apiigz <pazgastongonzalo@gmail.com>
2025-08-10 00:44:43 -0300

    Init
    Kevin gamer

---
+++ HUD.cs
@@ -0,0 +1,122 @@
+using Godot;
+using System;
+
+public partial class HUD : Control //=> Clase pública declarada. Heredamos la clase de Control
+{
+	//Tenemos tres variables con '[Export]', así que en el inspector de UI podemos cambiar estás tres variables.
+	[Export] public NodePath player_path; //=> Hacemos una "conexión" con la clase Player, a través del inspector
+	[Export] private NodePath ControlesJuego_path;//=> Hacemos lo mismo pero con el Panel de ControlesJuego
+	[Export] private NodePath PanelDialogo_path;//=> Hacemos lo mismo pero con el Panel de PanelDialogo
+	
+	//Declaramos los controladores gráficos y nodos principales (menos UI) como variables de manera privada.
+	private Player Jugador; //=> Player, el nodo principal 
+	private Panel ControlesJuego, PanelDialogo; //=> Los dos paneles (sirven como contenedores)
+	private ProgressBar BarradeHambre;//=> La barra de progreso
+	private Label LblInfo, LblCharla;//=> Los dos labels. Podemos ponerlos así para evitar muchas lineas de código
+	private Button BtnHablar, BtnExplorar, BtnJugar; //=> Los tres botones. Misma lógica que la de los labels
+	private Timer HambreTemporizador;//=> El temporizador
+	
+	//Inicializamos los controladores especificando su ubicación en el nodo
+	public override void _Ready()
+	{
+		//A cada variable le asignamos su tipo de controlador entre '<>' seguidos de su ubicación. Los nodos principales son su propia variable, como 'Player', 'Control', etc.
+		Jugador = GetNode<Player>(player_path);//=> En el inspector ya declaramos su ubicación.
+		ControlesJuego = GetNode<Panel>(ControlesJuego_path);//=> Lo mismo acá
+		PanelDialogo = GetNode<Panel>(PanelDialogo_path);//=> Lo mismo acá
+		
+		/*Las siguientes ubicaciones se dan según su posición en el nodo UI (este no se especifica)
+		Si estuvieran sueltas en el nodo UI, sin estar en ninguna ubicación seria (LblEjemplo)
+		Pero como están en un contenedor primero se pone este y luego el controlador => (Container/Label)
+		Si hubiera más de un container => (Container2/Container1/Label), y así...
+		*/
+		BarradeHambre = GetNode<ProgressBar>("ControlesJuego/BarradeHambre"); 
+		LblInfo = GetNode<Label>("ControlesJuego/LblInfo");
+		BtnHablar = GetNode<Button>("ControlesJuego/BtnHablar");
+		BtnExplorar = GetNode<Button>("ControlesJuego/BtnExplorar");
+		BtnJugar = GetNode<Button>("ControlesJuego/BtnJugar");
+		LblCharla = GetNode<Label>("PanelDialogo/LblCharla");
+		
+		/*Inicializamos la lógica de '¿Qué pasa?' cuando se llama a una función integrada de un controlador gráfico
+		En este caso .Pressed del controlador gráfico de los buttons. Hay que hacerlo para cada uno de los que están
+		'+=' es como un 'agregar', básicamente 'redirige'.
+		*/ 
+		BtnHablar.Pressed += PresionarHablar; //=> Cuándo se presiona se llama la función "PresionarHablar"
+		BtnExplorar.Pressed += PresionarExplorar;//=> Se llama la función PresionarExplorar
+		BtnJugar.Pressed += PresionarJugar;//=> Se llama la función PresionarJugar
+		
+		//Inicializamos las funciones incorporadas del temporizador
+		HambreTemporizador = GetNode<Timer>("../HambreTemporizador"); //=> Asiganamos su nodepath, No está dentro de UI, es su nodo hermano, así que va con (../) al principio
+		HambreTemporizador.WaitTime = 5.0; //=> .WaitTime es cada cuanto deja de contar
+		HambreTemporizador.Autostart = true; //=> .Autostart indica que se inicializa cuando se abre la escena o deja de contar
+		HambreTemporizador.Timeout += HambrePasivo;//=> Cuando deja de contar y que acción hace, en este caso devuelve una función que resta hambre
+		
+		//Funciones para que el botón de hablar haga más 'estético' el juego
+		PanelDialogo.GuiInput += ClickMousePaneldeDialogo; //=> Al PanelDialogo le llamamos la función incorporada de .GuiInput la función nombrada, que es del click del mouse. .GuiInput permite trabajar con el input del usuario (perifericos)
+		PanelDialogo.Visible = false; //=> Inicializamos al PanelDialogo con visibilidad false, ya que solo se mostrará si se habla
+	ActualizarUI(); //=> Función que actualiza los datos del hambre según lo que suceda
+	}
+	
+	//Funcion que se llama cuando se aprieta el botón de hablar
+	private void PresionarHablar() 
+	{
+		ControlesJuego.Visible = false; //=> La visibilidad de los controladores gráficos se pasa a false (era true)
+		PanelDialogo.Visible = true; //=> La visibilidad de los controladores gráficos del dialogo pasa a true (era false)
+		
+		string texto = Jugador.Hablar(); //=> Generamos una variable string que tendrá el dialogo que genere la función de Jugador.Hablar()
+		//PD = 'Jugador' incluye toda la clase del Nodo 'Player'. Podes acceder a sus métodos y funciones, las de Player.cs por qué lo declaraste al inicio, en el _Ready()
+		LblCharla.Text = texto; //=> Cambiamos el texto del label del PanelDialogo al dialogo que se generó.
+		//PD = Para cambiar el texto de un label se tiene que hacer referencia al .Text del label y no al label en si, no hagan 'LblCharla = Text', ya que es un error
+	}
+	
+	//Función que se llama cuando se aprieta el botón de explorar
+	private void PresionarExplorar()
+	{
+		//var es una variable que toma un tipo de dato según la situación, es cómo un 'comodin', le podes asignar cualquier tipo de dato.
+		var(exito, cantidad) = Jugador.Explorar(); //=> esta variable tendrá 'exito' y 'cantidad'. Explorar() devuelve un bool y un int respectivamente, de ahí a que creemos este var con dos variables y con esos nombres (exito = true o false, cantidad = entero) 
+		if (exito) //=> if (exito == true). Si no se le pone nada a un booleano en un if es por qué preguntamos si es true
+		{
+			//En un string, si antes de la primera comilla ponemos '$' nos permite ponerle variables dentro, siempre entre '{}'.
+			LblInfo.Text = $"¡Éxito, haz encontrado {cantidad} de comida!";
+		}
+		else //=> Si no es true...
+		{
+			LblInfo.Text = "No encontraste nada jijo...";
+		}
+		ActualizarUI(); //=> Actualiza el hambre. La cantidad si es reducida del hambre
+	}
+	
+	//Función que se llama cuando se aprieta el botón de jugar
+	private void PresionarJugar()
+	{
+		LblInfo.Text = Jugador.Jugar(); //=> .Jugar() devuelve un string, entonces se lo podemos asignar a un .Text
+		ActualizarUI(); //=> Actualizamos con el hambre generada por jugar
+	}
+	
+	//Mécanica que hace que a medida que pase el tiempo se reduzca el hambre. PD = Si se reduce el hambre es 'malo'
+	private void HambrePasivo()
+	{
+		Jugador.Hambre = Math.Max(0, Jugador.Hambre - 1); //Restamos una unidad de hambre cuando se llame. (En el temporizador, en el _Ready() de esta clase)
+		//A la instancia de Jugador (Player.cs) no solo podemos llamar sus funciones, si no también que a sus variables 'globales' también
+		ActualizarUI(); //=> Actualizamos
+	}
+	
+	//Método que actualiza los valores del hambre del temporizador según los cambios que se hagan
+	private void ActualizarUI()
+	{
+		BarradeHambre.Value = Jugador.Hambre; //Al value de la barra del hambre le asignamos nuestro apetito 
+		//BarradeCordura.Value = Jugador.Cordura; etc...
+	}
+
+	//Método que se llama cuando el jugador aprieta click con el mouse en el panel de dialogo (el LblCharla del PanelDialogo)
+	private void ClickMousePaneldeDialogo(InputEvent @event) //InputEvent permite almacenar variables de eventos relacionados al gui (perifericos)
+	{
+		if (@event is InputEventMouseButton MouseClick && MouseClick.Pressed) //=> traducido = 'Si el evento es un click de mouse (le ponemos un nombre) y este es presionado...' (suena down, lo se)
+		{
+			PanelDialogo.Visible = false; //El PanelDialogo cambia su visibilidad a false, cerrandose
+			ControlesJuego.Visible = true; //Los controladores del juego (botones y demás) vuelven a aparecer
+			
+		}
+	}
+	
+	//Cualquier duda me preguntan.
+}
---
+++ Player.cs
@@ -0,0 +1,92 @@
+//Librerias que permiten manejar el motor (Godot) y el lenguaje (C#).
+using Godot;
+using System;
+
+public partial class Player : Node2D //=> Clase pública declarada, partimos de Player y 'heredamos' la clase Node2D
+{
+	
+	//PD: Al final de cada linea de código se cierra con ';'. Las funciones, métodos y statements (bucles, ifs, etc.) se abren y cierran con '{}'
+	
+	//Hambre y su valor máximo en una variable aparte. Se pueden cambiar en el inspector de Player al tener '[Export]'
+	[Export] public int HambreMax  {get; set;} = 100; 
+	[Export] public int Hambre {get; set;} = 80;
+	//Variable para generar un numero aleatorio y usarlo en todo el programa
+	private RandomNumberGenerator rng;
+	//Variable que guarda el indice [i] del vector de los dialogos. Evita dos (o más) repeticiones de dialogo.
+	private int IndiceDialogoAnterior = -1;
+	
+	//Inicialización de variables, en este caso solo el de random, ya que afecta otras funciones
+	public override void _Ready()
+	{
+		rng = new RandomNumberGenerator();
+		rng.Randomize();
+	}
+	
+	/*Función que trabaja con la lógica de la exploración. Devuelve un bool (si encontró o no comida) 
+	y un int (que especifica cuánta cómida se encontro)*/
+	public (bool, int) Explorar()
+	{
+		//Una lógica de "tirada de dados", donde se trabajan las posibilidades del éxito de la exploración
+		int Roll = (int)rng.RandiRange(0, 100); // => Generamos un entero que devuelva un valor entre 0 y 100
+		int ChanceBase = 60; // => Chances de que no se pueda encontrar comida. Disminuirlo aumenta la dificultad.
+		
+		//Si el entero generado es menor que las chances iniciales la exploración fue un éxito.
+		if (Roll < ChanceBase)
+		{
+			int ComidaEncontrada = (int)rng.RandiRange(8, 25); //=> Un número entre el 8 y el 25 que dirá cuanta comida encontraste
+			Hambre = Math.Min(HambreMax, Hambre + ComidaEncontrada); /*=> A nuestra hambre le agregamos la comida
+																	Y con Math.Min redondeamos al entero más chico*/
+			return(true, ComidaEncontrada); //=> Devolvemos el resultado (true = si se encontró comida, ComidaEncontrada = valor hallado)
+		}
+		//Si no...
+		else
+		{
+			int Costo = (int)rng.RandiRange(2, 6); //=> Chavito, el viaje a Acapulco no fue gratis... (cuanta hambre nos costo)
+			Hambre = Math.Max(0, Hambre - Costo); //=> A nuestra hambre actual le restamos lo que nos costó. Math.Max devuelve el entero redondeado más alto.
+			return(false, 0);// => Devolvemos el resultado (false = no se encontró comida, 0 = valor hallado)
+		}
+		/*PD: Si el hambre sube es "bueno", ya que estariamos comiendo.
+			  Si disminuye es "malo", ya que no estariamos comiendo. No se compliquen tanto en esta lógica.*/ 
+	}
+	
+	//Función que devuelve un string a la hora de jugar con la mina. Jugar cuesta hambre.
+	public string Jugar()
+	{
+		int Costo = (int)rng.RandiRange(3, 6);//=> Calculamos cuanto nos costó jugar
+		Hambre = Math.Max(0, Hambre - Costo); //=> Lo que nos costó lo restamos del hambre
+		return $"Jugaron y se divirtieron jijo. Jugar costó {Costo} de hambre";//=> Devolvemos el string con los datos
+	}
+	
+	//Función que devuelve un string a la hora de hablar con la mina.
+	public string Hablar()
+	{
+		string[] Dialogos = new string[]{ //=> Un vector de una dimensión que almacena dialogos.
+			"Holii",
+			"Desearía poder volver a casa...",
+			"¿Cómo estás?",
+			"Moxas me debe plata...",//Esto es una versión arcaica. Se pasará todo a una base de datos
+			"¿Maxi? ah, ya te recuerdo...",
+			"Kevin, ¿dónde estabas cuando te necesitaba?",
+			"Escocia... aún me pregunto por qué...",
+			"Me divierto mucho con vos.", //La base de datos estará hecha con SQLite 
+			"No creo haberte dicho que pudieras hablarme.",
+			"Perón me quito muchas cosas...",//Hay una pequeña versión en los archivos locales de este proyecto
+			"Ahora que lo pienso, le debo mi vida al general.",//Se llama "DB_Game.dbb.
+			"Un otaku, wtf...",
+			"Matate y grabalo.",//La conexión a esta se hará con un NuGet (libreria) ya instalada por el Api.
+			"Daffcan... hace mucho no escuchaba ese nombre...",
+			"¿Que querés qué?",
+			"Extraño estar con vos...",
+			"Estoy aún en desarrollo :3",
+		}; 
+		int i; //=> Indice para posicionarnos en el vector
+		do //=> Lógica 'do - while' para generar un indice distinto al anterior, y evitar repeticiones seguidas 
+		{
+			i = (int)rng.RandiRange(0, Dialogos.Length - 1);//PD .RandiRange(int, int) te devuelve un número aleatorio entre un rango de dos enteros
+		}
+		while(i == IndiceDialogoAnterior && Dialogos.Length > 0);//Hacer el Do mientras el indice sea igual al indice anterior, guardado globalmente con anterioridad
+		
+		IndiceDialogoAnterior = i; //Cuándo se rompe el bucle se guarda el indice nuevo en está variable, para que la próxima vez no se repita este dialogo
+		return Dialogos[i]; //Devolvemos el string. Especificamos el indice para no devolver un vector.
+	}
+}
