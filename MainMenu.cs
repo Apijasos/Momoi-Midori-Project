625a56dc2d41c00892f9aa31cc9ab369a4e73120
Máximo Larocca David <89537@sistemas.frc.utn.edu.ar>
2025-08-14 15:49:17 -0300

    New branch, added volume config and fixed paths

--- MainMenu.cs
+++ MainMenu.cs
@@ -5,102 +5,119 @@ using System;
 
 public partial class MainMenu : Control
 {
-	//Path hacía el VBoxContainer de ControladoresPrincipales (se tiene que asignar en el inspector de la derecha)
-	[Export] private NodePath ControladoresPrincipales_path;
-	[Export] private NodePath PnlOpciones_path;
-	[Export] private NodePath PnlCargarPartida_path;
-	
-	//Los controladores gráficos, y el VBoxContainer, declarados como variables
-	private VBoxContainer ControladoresPrincipales, ControladoresCargarPartida, ControladoresOpciones;
-	private Button BtnJugar, BtnCargarPartida, BtnOpciones, BtnSalir, BtnOpcion1, BtnOpcion2, BtnCargar1, BtnCargar2;
-	private Label LblOpcion, LblCargarPartida;
+	// No hace falta exportar los NodePath si buscas por nombre fijo
+	private VBoxContainer ControladoresPrincipales;
+	private Button BtnJugar, BtnCargarPartida, BtnOpciones, BtnSalir, BtnCargarPartida1, BtnCargarPartida2;
 	private Panel PnlOpciones, PnlCargarPartida;
+	private Button BtnOpcion1, BtnOpcion2;
+	private Label LblOpciones, LblCargarPartida;
 	private HSlider VolumeSlider;
+	private AudioStreamPlayer AudioStreamPlayer;
 	
 	//Constructor de los anteriores atributos, y reasignación del método .Pressed() de dos de los cuatro botones (por ahora)
 	public override void _Ready()
 	{
-		//Asignación de los paths de los controladores gráficos. El primero a través del inspector y el resto escribiendo su dirección con los nodos
-		ControladoresPrincipales = GetNode<VBoxContainer>(ControladoresPrincipales_path);
-		PnlOpciones = GetNode<Panel>(PnlOpciones_path);
-		PnlCargarPartida = GetNode<Panel>(PnlCargarPartida_path);
-		
-		ControladoresOpciones = GetNode<VBoxContainer>("PnlOpciones/ControladoresOpciones");
-		ControladoresCargarPartida = GetNode<VBoxContainer>("PnlCargarPartida/ControladoresCargarPartida"); 
-		BtnJugar = GetNode<Button>("ControladoresPrincipales/BtnJugar");
-		BtnCargarPartida = GetNode<Button>("ControladoresPrincipales/BtnCargarPartida");
-		BtnOpciones = GetNode<Button>("ControladoresPrincipales/BtnOpciones");
-		BtnSalir = GetNode<Button>("ControladoresPrincipales/BtnSalir");
-		BtnOpcion1 = GetNode<Button>("ControladoresOpciones/BtnOpcion1");
-		BtnOpcion2 = GetNode<Button>("ControladoresOpciones/BtnOpcion2");
-		BtnCargar1 = GetNode<Button>("ControladoresCargarPartida/BtnOpcion1");
-		BtnCargar2 = GetNode<Button>("ControladoresCargarPartida/BtnOpcion2");
-		VolumeSlider = GetNode<HSlider>("ControladoresOpciones/VolumeSlider");
-		
-		//Declaro que pasa cuando se presionan BtnJugar y BtnSalir
+		// Inicialización usando los nombres exactos del árbol
+		ControladoresPrincipales = GetNode<VBoxContainer>("ControladoresPrincipales");
+		BtnJugar = ControladoresPrincipales.GetNode<Button>("BtnJugar");
+		BtnCargarPartida = ControladoresPrincipales.GetNode<Button>("BtnCargarPartida");
+		BtnOpciones = ControladoresPrincipales.GetNode<Button>("BtnOpciones");
+		BtnSalir = ControladoresPrincipales.GetNode<Button>("BtnSalir");
+
+		PnlOpciones = GetNode<Panel>("PnlOpciones");
+		BtnOpcion1 = PnlOpciones.GetNode<Button>("BtnOpcion1");
+		BtnOpcion2 = PnlOpciones.GetNode<Button>("BtnOpcion2");
+		LblOpciones = PnlOpciones.GetNode<Label>("LblOpciones");
+		VolumeSlider = PnlOpciones.GetNode<HSlider>("VolumeSlider");
+
+		PnlCargarPartida = GetNode<Panel>("PnlCargarPartida");
+		BtnCargarPartida1 = PnlCargarPartida.GetNode<Button>("BtnCargarPartida1");
+		BtnCargarPartida2 = PnlCargarPartida.GetNode<Button>("BtnCargarPartida2");
+		LblCargarPartida = PnlCargarPartida.GetNode<Label>("LblCargarPartida");
+
+		AudioStreamPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
+
+		// Conectar eventos de botones
 		BtnJugar.Pressed += PresionarJugar;
-		BtnSalir.Pressed += PresionarSalir;
-		BtnOpciones.Pressed += PresionarOpciones;
 		BtnCargarPartida.Pressed += PresionarCargarPartida;
-		
+		BtnOpciones.Pressed += PresionarOpciones;
+		BtnSalir.Pressed += PresionarSalir;
+		BtnOpcion1.Pressed += _on_btn_opcion_1_pressed;
+		BtnOpcion2.Pressed += _on_btn_opcion_2_pressed;
+		BtnCargarPartida1.Pressed += _on_btn_cargar_partida_1_pressed;
+		BtnCargarPartida2.Pressed += _on_btn_cargar_partida_2_pressed;
+		VolumeSlider.ValueChanged += _on_VolumeSlider_value_changed;
+
+		// Inicialización visual
 		PnlOpciones.Visible = false;
 		PnlCargarPartida.Visible = false;
-		
-		var slider = GetNode<HSlider>("PnlOpciones/VolumeSlider");
-		slider.Value = 100; // Inicializa a la derecha (máximo)
-		_on_VolumeSlider_value_changed(100); // Aplica el volumen máximo
-		
+
+		// Inicialización del slider de volumen y audio
+		VolumeSlider.Value = 100;
+		_on_VolumeSlider_value_changed(VolumeSlider.Value);
+
+		// Debug
+		GD.Print("Inicialización completa del menú principal.");
 	}
 	
 	//Metodo para abrir la escena principal del juego
-	private void PresionarJugar()
-	{
-		GetTree().ChangeSceneToFile("res://Main.tscn");
-	}
+	private void PresionarJugar() => GetTree().ChangeSceneToFile("res://Main.tscn");
 	
 	//Metodo para salir, si, es una boludez, podes simplemente asignarlo así: 'BtnSalir.Pressed += GetTree().Quit(); pero lo usamos de base para agregar otras cosas, como un cuadro de dialogo de confirmación
-	private void PresionarSalir()
-	{
-		GetTree().Quit();
-	}
+	private void PresionarSalir() => GetTree().Quit();
 	
 	private void PresionarOpciones()
 	{
-		PnlOpciones.Visible = true;
+		if(PnlOpciones.Visible==false)
+		{
+			PnlOpciones.Visible = true; 
+			PnlCargarPartida.Visible = false;
+		} 
+		else 
+		{
+			PnlOpciones.Visible = false; 
+			PnlCargarPartida.Visible = false;
+		}
+	}
+	
+	private void PresionarCargarPartida() 
+	{
+		if(PnlCargarPartida.Visible==false) 
+		{
+			PnlCargarPartida.Visible = true; 
+			PnlOpciones.Visible = false;
+		} 
+		else
+		{
+			PnlCargarPartida.Visible = false; 
+			PnlOpciones.Visible = false;
+		}
+	}
+	
+	private void _on_btn_opcion_1_pressed() => PnlOpciones.Visible = false;
+	
+	private void _on_btn_opcion_2_pressed() => PnlOpciones.Visible = false;
+	
+	private void _on_VolumeSlider_value_changed(double value)
+	{
+		float minDb=-80f;
+		float maxDb=0f;
 		
+		float sliderValue=(float)value;
+		
+		float volumeDb;
+		
+		if(sliderValue <= 0){
+			volumeDb = minDb; // Silencio
+		} else {
+		volumeDb = Mathf.Lerp(minDb, maxDb, (float)Math.Log10(sliderValue / 100f * 9 + 1));
+		}
+		
+		AudioStreamPlayer.VolumeDb = volumeDb;
 	}
 	
-	private void PresionarCargarPartida()
-	{ 	
-		PnlCargarPartida.Visible = true;
-	}
+	private void _on_btn_cargar_partida_1_pressed() => PnlCargarPartida.Visible = false;
 	
-	private void _on_btn_opcion_1_pressed()
-	{
-		PnlOpciones.Visible = false;
-	}
-	
-	private void _on_btn_opcion_2_pressed()
-	{
-		PnlOpciones.Visible = false;
-	}
-	
-	private void _on_VolumeSlider_value_changed(float value)
-	{
-		var audioPlayer = GetNode<AudioStreamPlayer>("AudioStreamPlayer");
-		float volumeDb = Mathf.Lerp(-80, 0, value / 100f);
-		audioPlayer.VolumeDb = volumeDb;
-	}
-	
-	
-	private void _on_btn_cargar_partida_1_pressed()
-	{
-		PnlCargarPartida.Visible = false;
-	}
-	
-	private void _on_btn_cargar_partida_2_pressed()
-	{
-		PnlCargarPartida.Visible = false;
-	}
+	private void _on_btn_cargar_partida_2_pressed() => PnlCargarPartida.Visible = false;
 }
 	
